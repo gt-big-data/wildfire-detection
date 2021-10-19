0587cbe33c57748103dc1705234b0b62fb47cc01
rluk3 <rluk3@gatech.edu>
2021-10-19 15:45:50 -0400

    initial website setup

---
+++ wildfire/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Initial website </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
