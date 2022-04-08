335d0430e7c553ca42faa7e3a95508e96a4ad9bc
PL3B3 <kevinqiesq@gmail.com>
2022-04-08 13:23:16 -0400

    + data documentation for cimis and zip wind
    + better column name for cimis wind

--- README.md
+++ README.md
@@ -17,3 +17,30 @@ Note that if the dependencies change, requirements.txt must be manually generate
 ## Linting
 Style checking is done with flake8 (one of the packages installed above). 
 Should automatically be configured for you
+
+
+# Data Dictionary
+
+## [CIMIS Wind](https://data.ca.gov/dataset/cimis-weather-station-data1/resource/32b3ba24-7ce8-461e-9812-95c739742c86)
+Data - data/cimis_wind.csv
+
+Code - preprocessing/platform/cimis_wind.py
+
+Code - preprocessing/cimis_wind.ipynb
+
+- wind_mph - average wind speed 
+- year - [2010, 2020] inclusive
+- lat
+- lon
+
+
+## [Wind By Zip](http://www.usa.com/rank/california-state--average-wind-speed--zip-code-rank.htm?yr=9000&dis=&wist=&plow=&phigh=)
+Based on 2010-2014 census data only
+
+Data - data/wind_by_zip.csv
+
+Code - preprocessing/platform/wind_by_zip.py
+
+- wind_mph
+- lat
+- lon
--- preprocessing/platform/wind_by_zip.py
+++ preprocessing/platform/wind_by_zip.py
@@ -2,15 +2,15 @@ import pandas as pd
 
 
 def get_ca_zip_latlon_csv():
-    zip_df = pd.read_csv('data\\staging\\zip\\zip_latlon.csv')
+    zip_df = pd.read_csv('data/staging/zip/zip_latlon.csv')
     zip_df = zip_df[zip_df['zip'].between(90001, 96162)]
     zip_df.reset_index()
-    zip_df.to_csv('data\\staging\\zip\\ca_zip_latlon.csv')
+    zip_df.to_csv('data/staging/zip/ca_zip_latlon.csv')
 
 
 def generate_wind_by_zip_csv():
     df = None
-    with open('data\\staging\\zip\\wind_by_zip.txt') as f:
+    with open('data/staging/zip/wind_by_zip.txt') as f:
         speed_by_zip = [
             [line.split()[1], line.split()[3]]
             for line in f.readlines()
@@ -20,7 +20,7 @@ def generate_wind_by_zip_csv():
         df.columns = ['wind_mph', 'zip']
         df = df.astype({'zip': 'int64'})
 
-        zip_df = pd.read_csv('data\\staging\\zip\\ca_zip_latlon.csv')
+        zip_df = pd.read_csv('data/staging/zip/ca_zip_latlon.csv')
 
         df = df.merge(zip_df, on='zip', how='left')
         df = df[['wind_mph', 'lat', 'lon']]
@@ -28,7 +28,7 @@ def generate_wind_by_zip_csv():
 
         print(df)
 
-        df.to_csv("data\\wind_by_zip.csv")
+        df.to_csv("data/wind_by_zip.csv")
 
 
 generate_wind_by_zip_csv()
