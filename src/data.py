f192470e0dedb783cbb7d7bcd1546e609230885b
PL3B3 <kevinqiesq@gmail.com>
2022-04-20 19:51:54 -0400

    + v2 predictions data

--- src/data.py
+++ src/data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import json
 import random
 
+DATA_PATH = 'analysis/final_demo_analysis/wildfire_predictions_v2.csv'
+
 
 def get_mock_data():
     locations = json.load(open('data/locations.json'))
@@ -17,7 +19,7 @@ def get_mock_data():
 
 def get_wildfire_data():
     df = pd.read_csv(
-        'analysis/final_demo_analysis/wildfire_predictions.csv',
+        DATA_PATH,
         index_col=0
     )
     df.columns = ['lat', 'lon', 'fire_prob']
